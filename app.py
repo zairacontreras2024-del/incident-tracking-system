ce6799a2ae15a706e2edc723478be9d9a18ed86d
zairacontreras2024-del <zaira.contreras2024@gmail.com>
2026-06-07 18:39:43 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+print("Incident Tracking System")
+
+incident_id = 1001
+employee = "John Smith"
+incident_type = "Near Miss"
+
+print(f"Incident ID: {incident_id}")
+print(f"Employee: {employee}")
+print(f"Type: {incident_type}")
